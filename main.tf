a7a509598ac915c70f0eb1a34a9d7b48415de44d
Andrii Penchuk <penja7@gmail.com>
2024-09-26 15:54:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,10 @@
-# Configure the backend with S3 and DynamoDB for state locking
-locals {
-  bucket_name = "ape-terraform-state-bucket"
-  dynamodb_table_name = "terraform-locks"
-}
-
-
 terraform {
   backend "s3" {
-    bucket         = local.bucket_name
+    bucket         = "ape-terraform-state-bucket"
     key            = "global/s3/terraform.tfstate"  # Path in S3 where the state file will be stored
     region         = "us-west-2"
     encrypt        = true                           # Enables server-side encryption
-    dynamodb_table = local.dynamodb_table_name      # Name of the DynamoDB table for state locking
+    dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
   }
 }
 
