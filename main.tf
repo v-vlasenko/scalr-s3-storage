af5d66d8a479372c05875e9ed89ef568383325ce
Andrii Penchuk <penja7@gmail.com>
2024-09-27 16:34:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend “s3” {
+    bucket         = “ape-terraform-state-bucket”
+    key            = “global/s3/terraform.tfstate”  # Path in S3 where the state file will be stored
+    region         = “us-west-2"
+    encrypt        = true                           # Enables server-side encryption
+    dynamodb_table = “terraform-locks”      # Name of the DynamoDB table for state locking
+  }
+}
+
+
 resource "null_resource" "test" {
   triggers = {
     always_run = timestamp()
