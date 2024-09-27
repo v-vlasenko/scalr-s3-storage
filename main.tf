32eded7488fc7007020abe6d865ca696f0e4d3ae
Andrii Penchuk <penja7@gmail.com>
2024-09-27 16:36:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend “s3” {
-    bucket         = “ape-terraform-state-bucket”
-    key            = “global/s3/terraform.tfstate”  # Path in S3 where the state file will be stored
-    region         = “us-west-2"
+  backend "s3" {
+    bucket         = "ape-terraform-state-bucket"
+    key            = "global/s3/terraform.tfstate"  # Path in S3 where the state file will be stored
+    region         = "us-west-2"
     encrypt        = true                           # Enables server-side encryption
-    dynamodb_table = “terraform-locks”      # Name of the DynamoDB table for state locking
+    dynamodb_table = "terraform-locks"      # Name of the DynamoDB table for state locking
   }
 }
 
